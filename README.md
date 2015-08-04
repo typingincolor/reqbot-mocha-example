d8f49f7fcb82cefc49ec2c1b11709cc9975f2d4a
Andrew Braithwaite <abraithw@gmail.com>
2015-08-04 21:42:23 +0100

    more promises

---
+++ README.md
@@ -0,0 +1,11 @@
+# Reqbot Mocha Example
+
+This project shows how to use [reqbot](https://github.com/typingincolor/reqbot)
+in a mocha test...
+
+to run:
+
+```bash
+npm install
+npm test
+```
--- test.js
+++ test.js
@@ -28,23 +28,34 @@ var create_response = function() {
   return deferred.promise;
 };
 
+var get_response = function(uuid) {
+  var deferred = Q.defer();
+  request.get("https://reqbot-api.herokuapp.com/mocha_test")
+    .set('X-REQBOT-RESPONSE', uuid)
+    .end(function(err, res) {
+      console.info("hello");
+      if (!!err) {
+        console.info("error", err);
+        deferred.reject(err);
+      } else {
+        deferred.resolve(res);
+      }
+    });
+  return deferred.promise;
+};
+
 describe('Example reqbot test', function() {
   it('should POST a response and recieve it back', function(done) {
     create_response().then(function(uuid) {
-      request.get("https://reqbot-api.herokuapp.com/mocha_test")
-        .set('X-REQBOT-RESPONSE', uuid)
-        .end(function(err, res) {
-          console.info("hello");
-          if (!!err) {
-            console.info("error", err);
-          } else {
-            expect(res).to.be.text;
-            expect(res.status).to.equal(200);
-            expect(res.text).to.equal("this is a body");
-            expect(res.header.header1).to.equal("value1");
-            done();
-          }
-        });
+      return get_response(uuid);
+    }).then(function(result) {
+      expect(result).to.be.text;
+      expect(result.status).to.equal(200);
+      expect(result.text).to.equal("this is a body");
+      expect(result.header.header1).to.equal("value1");
+      done();
+    }).fail(function(err) {
+      done(err);
     });
   });
 });
